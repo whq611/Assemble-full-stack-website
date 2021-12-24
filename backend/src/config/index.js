4d79d95d15245ed337d51813c4452e2f622937bf
whq611 <85942669+whq611@users.noreply.github.com>
2021-12-24 17:04:30 -0600

    Add files via upload

---
+++ backend/src/config/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable global-require */
+module.exports = {
+  dbConfig: require('./db'),
+  authConfig: require('./auth'),
+  appConfig: require('./app'),
+};
+/* eslint-enable global-require */
